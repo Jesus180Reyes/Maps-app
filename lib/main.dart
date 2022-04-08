441dccbc62678bc7e7d904430c4d32ceacc5affd
Jesus Reyes <luisdejesus200122@gmail.com>
2022-04-07 20:31:45 -0600

    Proyecto Maps-App

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:map_app/pages/screens.dart';
+
+void main() => runApp(const MapsApp());
+
+class MapsApp extends StatelessWidget {
+  const MapsApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Maps App',
+      home: LoadingPage(),
+    );
+  }
+}
---
+++ lib/pages/screens.dart
@@ -0,0 +1,3 @@
+export 'package:map_app/pages/gps_access_page.dart';
+export 'package:map_app/pages/map_page.dart';
+export 'package:map_app/pages/loading_page.dart';
